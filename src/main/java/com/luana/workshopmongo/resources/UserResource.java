c72dbeb01a44284a42d9c4756a78802e6eb468e9
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-09 22:34:08 -0300

    Connecting MongoDB to repository and service

--- src/main/java/com/luana/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luana/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.luana.workshopmongo.resources;
 
 import com.luana.workshopmongo.domain.User;
+import com.luana.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,12 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1001", "Maria Brown", "maria@gmail.com");
-        User alex = new User("1002", "Alex Green", "alex@gmail.com");
-        List<User>list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
