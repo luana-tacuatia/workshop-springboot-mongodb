3fba06e5a98a42958bbcaf2d4d4c0bda5cddde58
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-09 23:29:21 -0300

    feat: getting a user by id

--- src/main/java/com/luana/workshopmongo/services/UserService.java
+++ src/main/java/com/luana/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.luana.workshopmongo.services;
 
 import com.luana.workshopmongo.domain.User;
 import com.luana.workshopmongo.repository.UserRepository;
+import com.luana.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
---
+++ src/main/java/com/luana/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.luana.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException (String msg){
+
+    }
+
+}
