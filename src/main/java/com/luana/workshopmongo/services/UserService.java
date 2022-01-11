4f53613442b3ab0b279694370bd917a3ab3ca179
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-10 23:32:00 -0300

    feat: inserting user using POST

--- src/main/java/com/luana/workshopmongo/services/UserService.java
+++ src/main/java/com/luana/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.luana.workshopmongo.services;
 
 import com.luana.workshopmongo.domain.User;
+import com.luana.workshopmongo.dto.UserDTO;
 import com.luana.workshopmongo.repository.UserRepository;
 import com.luana.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,20 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
