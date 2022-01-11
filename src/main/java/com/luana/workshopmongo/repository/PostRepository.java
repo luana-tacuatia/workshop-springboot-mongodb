51b3955c464b33a0debc12b02a8e73a10b37a395
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-11 18:18:57 -0300

    feat: creating entity Post using nested user

---
+++ src/main/java/com/luana/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.luana.workshopmongo.repository;
+
+import com.luana.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
