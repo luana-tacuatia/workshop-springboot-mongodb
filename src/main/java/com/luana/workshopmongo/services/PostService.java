2e9e5076194176b3016d612372500b123f90927e
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-13 00:04:16 -0300

    feat: simple query using query methods

--- src/main/java/com/luana/workshopmongo/services/PostService.java
+++ src/main/java/com/luana/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.luana.workshopmongo.dto.UserDTO;
 import com.luana.workshopmongo.repository.PostRepository;
 import com.luana.workshopmongo.repository.UserRepository;
 import com.luana.workshopmongo.services.exception.ObjectNotFoundException;
+import org.apache.coyote.http11.upgrade.UpgradeServletOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
