d53418d8009ed39c61fbf4db678c891cb2461b24
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-12 22:06:44 -0300

    feat: getting a post by id

---
+++ src/main/java/com/luana/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.luana.workshopmongo.services;
+
+import com.luana.workshopmongo.domain.Post;
+import com.luana.workshopmongo.domain.User;
+import com.luana.workshopmongo.dto.UserDTO;
+import com.luana.workshopmongo.repository.PostRepository;
+import com.luana.workshopmongo.repository.UserRepository;
+import com.luana.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
